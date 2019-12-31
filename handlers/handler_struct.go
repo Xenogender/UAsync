bb04bde70544ee927412de16d86baf815f0b9af3
Xenogender <97792933+Xenogender@users.noreply.github.com>
2019-12-31 16:47:24 +0000

    4.1.0

--- handlers/handler_struct.go
+++ handlers/handler_struct.go
@@ -5,3 +5,6 @@ import "github.com/bwmarrin/discordgo"
 
 type HandlersProps struct {
 	msgPingPongHanlder func(s *discordgo.Session, m *discordgo.MessageCreate)
+	imgGenerator       func(s *discordgo.Session, m *discordgo.MessageCreate)
+	helpJavaHandler    func(s *discordgo.Session, m *discordgo.MessageCreate)
+	msgGreeting        func(s *discordgo.Session, m *discordgo.MessageCreate)
