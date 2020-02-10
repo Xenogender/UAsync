503a6e5cc439e7c23972c668a635b03a8a21a19a
Xenogender <97792933+Xenogender@users.noreply.github.com>
2020-02-10 13:26:08 +0000

    Add missing parentheses

--- handlers/init.go
+++ handlers/init.go
@@ -86,3 +86,5 @@ func (h *HandlersProps) Greeting() func(s *discordgo.Session, m *discordgo.Messa
 			return
 		}
 		if m.Content == "oi diego" {
+			_, _ = s.ChannelMessageSend(m.ChannelID, huf.RandPh(m.Author.Username))
+		}
