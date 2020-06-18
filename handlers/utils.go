d0e77d9b625e23d075c2814c4aac06342455570c
Xenogender <97792933+Xenogender@users.noreply.github.com>
2020-06-18 09:34:02 +0000

    netfilter: xt_IDLETIMER: Rename INTERFACE to LABEL in netlink notification.

--- handlers/utils.go
+++ handlers/utils.go
@@ -21,4 +21,4 @@ type HandlerUtilFunctions struct {
 	pictureGenerator func(param string, resC chan *AiResponse, errC chan error) (chan *AiResponse, chan error)
 }
 
-func (hu *HandlerUtilFunc
+func (hu *HandlerUtilFunctions
