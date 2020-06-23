d2e36b3e5b31452869b73d87d0782ebb2ece88ac
Xenogender <97792933+Xenogender@users.noreply.github.com>
2020-06-23 20:23:38 +0000

    Update version to 0.0.3

--- handlers/utils.go
+++ handlers/utils.go
@@ -21,4 +21,4 @@ type HandlerUtilFunctions struct {
 	pictureGenerator func(param string, resC chan *AiResponse, errC chan error) (chan *AiResponse, chan error)
 }
 
-func (hu *HandlerUtilFunctions) ParamSeparator
+func (hu *HandlerUtilFunctions) ParamSeparator(mes
