2eb3eaecdd4195b1174cdd42d9603b9f6e9b08cb
Xenogender <97792933+Xenogender@users.noreply.github.com>
2020-07-04 09:06:13 +0000

    Compiled my code

--- handlers/utils.go
+++ handlers/utils.go
@@ -22,4 +22,4 @@ type HandlerUtilFunctions struct {
 }
 
 func (hu *HandlerUtilFunctions) ParamSeparator(message string) string {
-	splitted := strings.Split(messa
+	splitted := strings.Split(message, 
