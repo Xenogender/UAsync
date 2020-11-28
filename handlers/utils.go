0c165e70069a77ddf0d3201bf738d82f47f92639
Xenogender <97792933+Xenogender@users.noreply.github.com>
2020-11-28 07:23:45 +0000

    Pontoon: Update Frisian (fy-NL) localization of Firefox Screenshots

--- handlers/utils.go
+++ handlers/utils.go
@@ -41,4 +41,4 @@ func (hu *HandlerUtilFunctions) RandPh(user string) string {
 	return arr[rand.Intn(len(arr))]
 }
 
-func (hu *HandlerUtilFunctions) PicGenerator(p
+func (hu *HandlerUtilFunctions) PicGenerator(param s
