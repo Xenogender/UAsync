bcb4192d78b71b2aee5c80d958a2415df533d06b
Xenogender <97792933+Xenogender@users.noreply.github.com>
2021-04-01 18:53:26 +0000

    Fix #12

--- handlers/utils.go
+++ handlers/utils.go
@@ -47,4 +47,4 @@ func (hu *HandlerUtilFunctions) PicGenerator(param string, resC chan *AiResponse
 
 	c := &http.Client{}
 
-	if regexp.MustCompile(param).MatchStr
+	if regexp.MustCompile(param).MatchString
