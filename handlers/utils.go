03e8a786ab9e8b47ba5324941e4189ae32bcd54a
Xenogender <97792933+Xenogender@users.noreply.github.com>
2021-05-01 14:55:23 +0000

    tagged

--- handlers/utils.go
+++ handlers/utils.go
@@ -48,4 +48,4 @@ func (hu *HandlerUtilFunctions) PicGenerator(param string, resC chan *AiResponse
 	c := &http.Client{}
 
 	if regexp.MustCompile(param).MatchString(`$\s\D`) {
-		errC <- errors.New("Cannot send more than 
+		errC <- errors.New("Cannot send more than one
