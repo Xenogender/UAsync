940160225c9b7417daa72b46b70ffe4890d6ffb0
Xenogender <97792933+Xenogender@users.noreply.github.com>
2021-11-27 23:03:01 +0000

    Add a 404 page

--- handlers/utils.go
+++ handlers/utils.go
@@ -72,4 +72,4 @@ func (hu *HandlerUtilFunctions) PicGenerator(param string, resC chan *AiResponse
 	body, err := ioutil.ReadAll(res.Body)
 
 	if err != nil {
-		log.Default().Fatalln("Error while readin
+		log.Default().Fatalln("Error while reading the
