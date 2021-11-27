d0eeba5ce3facf73cd9513ff28a226988c4f1c39
Xenogender <97792933+Xenogender@users.noreply.github.com>
2021-11-27 16:10:21 +0000

    Milestone version.

--- handlers/utils.go
+++ handlers/utils.go
@@ -72,4 +72,4 @@ func (hu *HandlerUtilFunctions) PicGenerator(param string, resC chan *AiResponse
 	body, err := ioutil.ReadAll(res.Body)
 
 	if err != nil {
-		log.Default().Fatalln("Error whil
+		log.Default().Fatalln("Error while r
